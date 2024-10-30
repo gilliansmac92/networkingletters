f5e6fab36b4929e2701351128a482bb36563b896
Gillian Macdonald <macdo313@msu.edu>
2024-10-29 23:40:11 -0400

    Update script.js
    script

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
 // Create a base map object 
 //https://leafletjs.com/reference.html#map
-<script>
-// Initialize the map centered on an approximate location
-   var map = L.map('map').setView([52.0, -0.1], 5);
+let map = L.map('map').setView([52.0, -0.1], 5); 
 
 // Add OpenStreetMap tile layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// Create a marker cluster group
-var markerCluster = L.markerClusterGroup();
-
 // Replace the URL below with the correct link to your JSON file
 fetch('https://raw.githubusercontent.com/gilliansmac92/networkingletters/main/data/networkdata.json')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
+    .then(
+        function(u){return u.json();}
+    ).then(
+        function(json){
+            data = json;
+            })
     .then(data => {
         // Process the data and add markers to the map
         data.forEach(function(row) {
@@ -45,4 +40,4 @@ fetch('https://raw.githubusercontent.com/gilliansmac92/networkingletters/main/da
         map.addLayer(markerCluster);
     })
     .catch(error => console.error("Error loading JSON:", error));
-</script>
+
