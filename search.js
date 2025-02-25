237c2cfada42a31ec6ba2f9b46f4363206d3d676
Gillian Macdonald <macdo313@msu.edu>
2025-02-25 16:23:31 -0500

    Update search.js

--- search.js
+++ search.js
@@ -1,1 +1,11 @@
+const search = document.getElementById('search'); const nav = document.getElementById('nav'); const links = nav.getElementsByTagName('a');
 
+search.addEventListener('keyup', function() { const query = search.value.toLowerCase();
+
+for (let i = 0; i < links.length; i++) { const text = links[i].textContent.toLowerCase();
+    if (text.includes(query)) {
+        links[i].style.display = 'block';
+      } else {
+        links[i].style.display = 'none';
+      }
+    } });
