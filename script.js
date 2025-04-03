df3ba3f7f733a1eceb6babb593a45679933d9f07
Gillian Macdonald <macdo313@msu.edu>
2025-04-03 16:14:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+ // Dropdown on mouse hover
+ $(document).ready(function () {
+    function toggleNavbarMethod() {
+        if ($(window).width() > 992) {
+            $('.navbar .dropdown').on('mouseover', function () {
+                $('.dropdown-toggle', this).trigger('click');
+            }).on('mouseout', function () {
+                $('.dropdown-toggle', this).trigger('click').blur();
+            });
+        } else {
+            $('.navbar .dropdown').off('mouseover').off('mouseout');
+        }
+    }
+    toggleNavbarMethod();
+    $(window).resize(toggleNavbarMethod);
+});
+
 // Create a base map object
 let map = L.map('map').setView([52.0, -0.1], 5); 
 
