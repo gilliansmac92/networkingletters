41cd2c0c07164541f8c5a2afea2b8188455d9f82
Gillian Macdonald <macdo313@msu.edu>
2025-04-03 16:23:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
- // Dropdown on mouse hover
- $(document).ready(function () {
-    function toggleNavbarMethod() {
-        if ($(window).width() > 992) {
-            $('.navbar .dropdown').on('mouseover', function () {
-                $('.dropdown-toggle', this).trigger('click');
-            }).on('mouseout', function () {
-                $('.dropdown-toggle', this).trigger('click').blur();
-            });
-        } else {
-            $('.navbar .dropdown').off('mouseover').off('mouseout');
-        }
-    }
-    toggleNavbarMethod();
-    $(window).resize(toggleNavbarMethod);
-});
+// dropdown menu
+var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
+  return new bootstrap.Dropdown(dropdownToggleEl)
+})
 
 // Create a base map object
 let map = L.map('map').setView([52.0, -0.1], 5); 
